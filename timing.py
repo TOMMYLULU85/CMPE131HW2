79440763468ce560cb8923192105a8c6b266b0b1
Tommy <tommyluu85@yahoo.com>
2021-03-01 11:16:48 -0800

    Created a code about calculating time with a decorator. It imports time in order to use time function. The decorator computes time of function and the print function prints out the statement. Used time.sleep(2) to test the code.

---
+++ timing.py
@@ -0,0 +1,19 @@
+import time
+def calculate_time(func):
+     '''This calculate time is here because its the main function'''
+     def wrapper():
+      '''You would need this function because it prints out time'''
+      x = time.time()
+      print(x)
+      func()
+     return wrapper
+
+@calculate_time
+
+def print_time():
+    '''The reason to write this function is because it prints out the phase'''
+    print('Total time X')
+
+
+print_time()
+time.sleep(2)
