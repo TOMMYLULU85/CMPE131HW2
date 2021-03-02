180e6a036ba1ac87d2835c4defc76fa99c8170ee
Tommy <tommyluu85@yahoo.com>
2021-03-01 21:33:37 -0800

    Got rid of print time and instead added a test time function in order to test how long it takes to write a function.

--- timing.py
+++ timing.py
@@ -3,20 +3,17 @@ def calculate_time(func):
      '''This calculate time is here because its the main function'''
      def wrapper():
       '''You would need this function because it prints out time'''
-      x = time.time()
-      time.sleep(2)
-      print(x)
+      X= time.time()
       func()
+      print(f"Total time {X}")
      return wrapper
 
 @calculate_time
+def test_time():
+      time.sleep(2)
+test_time()
 
-def print_time():
-    '''The reason to write this function is because it prints out the phase'''
-    print('Total time X')
 
-
-print_time()
 
 
 
